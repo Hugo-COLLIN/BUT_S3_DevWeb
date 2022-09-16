1c970225c187564bc8539bd39eba504dffe0a9ff
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-09-16 14:36:58 +0200

    Restructuring for TP5 exercise2

--- TP5/index.php
+++ TP5/index.php
@@ -1,20 +1,18 @@
 <?php
 
-require_once 'Renderer.php';
-require_once 'AudioTrack.php';
-require_once 'AlbumTrack.php';
-require_once 'PodcastTrack.php';
+require_once 'src/track/Renderer.php';
+require_once 'src/track/AudioTrack.php';
+require_once 'src/track/AlbumTrack.php';
+require_once 'src/track/PodcastTrack.php';
 
-require_once 'AudioTrackRenderer.php';
-require_once 'AlbumTrackRenderer.php';
-require_once 'PodcastTrackRenderer.php';
+require_once 'src/track/AudioTrackRenderer.php';
+require_once 'src/track/AlbumTrackRenderer.php';
+require_once 'src/track/PodcastTrackRenderer.php';
 
-
-require_once 'AlbumTrackRenderer.php';
 
 $track1 = new AlbumTrack('I\'m with you', 'audio/01-Im_with_you_BB-King-Lucille.mp3');
 $track2 = new AlbumTrack('I_Need_Your_Love', 'audio/02-I_Need_Your_Love-BB_King-Lucille.mp3');
-//$track3 = new AlbumTrack('Country_Girl', 'audio/03-Country_Girl-BB_King-Lucille.mp3');
+
 
 $track1->album = 'Lucille';
 $track1->auteur = 'B.B. King';
