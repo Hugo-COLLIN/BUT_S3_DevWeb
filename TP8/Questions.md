7c9c74714e5f94e85d434a01d294d1fcbd95d5dd
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-09-30 15:55:45 +0200

    TD8 exercises 1, 2, 3

---
+++ TP8/Questions.md
@@ -0,0 +1,13 @@
+# Exercice 1
+
+1) Dans le tableau, on regarde à parir de l'élément "REQUEST_METHOD" 
+qui est la méthode HTTP et les éléments suivants qui sont les headers.\
+"SERVER_NAME"\
+"SCRIPT_FILENAME"
+2) Il y a 51 élément dans $_SERVER dans le navigateur contre 54 dans 
+le terminal, et les chanps sont différents. \
+L'exécution avec le terminal se fait en local (pas de HTTP !) alors qu'on se connecte 
+à un serveur Apache dans le navigateur.
+
+
+# Exercice 2
--- TP8/TD8.php
+++ TP8/TD8.php
@@ -6,6 +6,28 @@
  * la temporisation de sortie avec la fonction ob_end_flush() au début du script.
  */
 
+//ob_end_flush();
+/*
+$_SERVER['QUERY_STRING'] = 'testo';
 echo '<pre>'; 
 var_dump($_SERVER);
+*/
+/*
+http_response_code("201");
+header("bla: blo");
+*/
+//ob_start();
 
+echo "<p>" . $_SERVER["REQUEST_METHOD"] . " " . $_SERVER["REQUEST_URI"] . " " . $_SERVER["SERVER_PROTOCOL"] . " " . http_response_code() . "</p>";
+
+$h = getallheaders();
+foreach ($h as $item => $value)
+    echo "<b>" . $item . " : </b>" . $value . "<br>";
+
+ //activer ob_start() pour que ça marche
+/*
+http_response_code("201");
+header("bla: blo");
+*/
+
+echo http_response_code();
