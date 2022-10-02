a5c21a55a8c1e082dfcceb3821006a839ebf80b7
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-10-02 13:30:37 +0200

    TP5 Composer autoloader import + composer.json created

---
+++ TP7/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'iutnc/deefy',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '7c9c74714e5f94e85d434a01d294d1fcbd95d5dd',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'iutnc/deefy' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '7c9c74714e5f94e85d434a01d294d1fcbd95d5dd',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
