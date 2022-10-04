72b0aaa11026e8e7a4eb10a740b15bb5a9888366
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-10-04 11:25:46 +0200

    TP8

--- TP8/TD8.php
+++ TP8/TD8.php
@@ -1,15 +1,15 @@
 <?php
 
 /*
- * Si la désactivation de la temporisation de sortie ne fonctionne pas avec le .htaccess 
- * fourni, il est possible de forcer l'envoi les données du tampon de sortie et d'éteindre 
+ * Si la désactivation de la temporisation de sortie ne fonctionne pas avec le .htaccess
+ * fourni, il est possible de forcer l'envoi les données du tampon de sortie et d'éteindre
  * la temporisation de sortie avec la fonction ob_end_flush() au début du script.
  */
 
 //ob_end_flush();
 /*
 $_SERVER['QUERY_STRING'] = 'testo';
-echo '<pre>'; 
+echo '<pre>';
 var_dump($_SERVER);
 */
 /*
