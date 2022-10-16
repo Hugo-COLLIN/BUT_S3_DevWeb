203d773b81ec1dfabba855b2a297fdbe81fb1337
Hugo COLLIN <hugo.collin1@outlook.fr>
2022-10-16 11:24:35 +0200

    reorganize folders + code understanding

--- TP11/index.php
+++ TP11/index.php
@@ -79,9 +79,36 @@ switch ($_GET['action'])
             END;
         else
         {
-            if (is_uploaded_file($_FILES['upload']['tmp_name']) && $_FILES['upload']['type'] === "audio/mp4")
+            //if (is_uploaded_file($_FILES['upload']['tmp_name']) && $_FILES['upload']['type'] === "audio/mp4")
+            if (isset($_FILES["upload"]) && $_FILES["upload"]["error"] === 0)
             {
+                $allowed = array("mp3" => "audio/mp3", "mpeg" => "audio/mpeg");
+                $pNom = $_FILES["upload"]["name"];
+                $pType = $_FILES["upload"]["type"];
+                $pSize = $_FILES["upload"]["size"];
 
+                // Vérifie l'extension du fichier
+                $ext = pathinfo($pNom, PATHINFO_EXTENSION);
+                if(!array_key_exists($ext, $allowed)) die("Erreur : Veuillez sélectionner un format de fichier valide.");
+
+                // Vérifie la taille du fichier - 5Mo maximum
+                $maxsize = 5 * 1024 * 1024;
+                if($pSize > $maxsize) die("Erreur: La taille du fichier est supérieure à la limite autorisée.");
+
+                // Vérifie le type MIME du fichier
+                if(in_array($pType, $allowed)){
+                    // Vérifie si le fichier existe avant de le télécharger.
+                    if(file_exists("audio/" . $_FILES["upload"]["name"])){
+                        echo $_FILES["upload"]["name"] . " existe déjà.";
+                    } else{
+                        move_uploaded_file($_FILES["upload"]["tmp_name"], "audio/" . $_FILES["upload"]["name"]);
+                        echo "Votre fichier a été téléchargé avec succès.";
+                    }
+                } else{
+                    echo "Error: Il y a eu un problème de téléchargement de votre fichier. Veuillez réessayer.";
+                }
+            } else{
+                $rend = "Pas de fichier";
             }
         }
 
